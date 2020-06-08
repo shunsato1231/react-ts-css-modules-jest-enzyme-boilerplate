91c2cfe93ce83e0883c681715eddd09f02bba56e
sato-shn <sato-shn@ID-PC196.local>
2020-06-08 09:22:50 +0900

    setup jest

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+const path = require("path")
+
+module.exports = {
+    moduleFileExtensions: [
+        'ts',
+        'tsx',
+        'js'
+    ],
+    transform: {
+        '^.+\\.(js|jsx|mjs)$': 'babel-jest',
+        '^.+\\.(ts|tsx)$': 'ts-jest'
+    },
+    setupFiles: [
+        '<rootDir>/config/jest/setupEnzyme.ts'
+    ],
+    globals: {
+        'ts-jest': {
+            babelConfig: true,
+            tsConfig: path.resolve(__dirname, "tsconfig.json")
+        }
+    },
+}
